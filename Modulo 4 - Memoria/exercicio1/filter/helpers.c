f10b9d43f5113720396bf9ea135f22e77022cdd8
Alefer Reinert <aleferreinert@gmail.com>
2022-07-29 20:08:45 -0300

    Solucionado filtro de blur do exercicio1.

--- Modulo 4 - Memoria/exercicio1/filter/helpers.c
+++ Modulo 4 - Memoria/exercicio1/filter/helpers.c
@@ -55,6 +55,50 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Cópia da imagem para usar no cálculo do blur
+    RGBTRIPLE copy[height][width];
+    for (int row = 0; row < height; row++)
+    {
+        for (int column = 0; column < width; column++)
+        {
+            copy[row][column] = image[row][column];
+        }
+    }
+
+    // Blur
+    for (int row = 0; row < height; row++)
+    {
+        for (int column = 0; column < width; column++)
+        {
+            float qtd_pixels = 0; // Quantidade de pixels existentes
+            float r = 0;          // Soma de vermelho dos pixels existentes
+            float g = 0;          // Soma de verde dos pixels existentes
+            float b = 0;          // Soma de azul dos pixels existentes
+
+            /*  Verifica cada pixel ao redor do pixel atual (como uma caixa 3x3).
+                Se o pixel estiver dentro dos limites da imagem:
+                    Atualiza a quantidade de pixels
+                    Pega a quantidade de cada cor e adiciona nas variáveis acima para fazer a soma
+            */
+            for (int box_row = row - 1; box_row <= (row + 1); box_row++)
+            {
+                for (int box_column = column - 1; box_column <= (column + 1); box_column++)
+                {
+                    if (box_row >= 0 && box_column >= 0 && box_row < height && box_column < width)
+                    {
+                        qtd_pixels++;
+                        r += copy[box_row][box_column].rgbtRed;
+                        g += copy[box_row][box_column].rgbtGreen;
+                        b += copy[box_row][box_column].rgbtBlue;
+                    }
+                }
+            }
+            // Atualiza o pixel atual com os novos valores
+            image[row][column].rgbtRed = round(r / qtd_pixels);
+            image[row][column].rgbtGreen = round(g / qtd_pixels);
+            image[row][column].rgbtBlue = round(b / qtd_pixels);
+        }
+    }
     return;
 }
 
