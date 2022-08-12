3802e396b0b4817beefbda9cdbd3cca3711e2f76
Alefer Reinert <aleferreinert@gmail.com>
2022-08-11 23:15:28 -0300

    Solucionado Lab4 Volume.

---
+++ Modulo 4 - Memoria/lab4/volume.c
@@ -0,0 +1,51 @@
+// Modifies the volume of an audio file
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Number of bytes in .wav header
+const int HEADER_SIZE = 44;
+
+int main(int argc, char *argv[])
+{
+    // Check command-line arguments
+    if (argc != 4)
+    {
+        printf("Usage: ./volume input.wav output.wav factor\n");
+        return 1;
+    }
+
+    // Open files and determine scaling factor
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    FILE *output = fopen(argv[2], "w");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    // Copy header
+    int8_t header[HEADER_SIZE];
+    fread(header, sizeof(int8_t), HEADER_SIZE, input);
+    fwrite(header, sizeof(int8_t), HEADER_SIZE, output);
+
+    // Copy samples
+    int16_t sample;
+    float factor = atof(argv[3]);
+    while (fread(&sample, sizeof(int16_t), 1, input) == 1)
+    {
+        sample *= factor;
+        fwrite(&sample, sizeof(int16_t), 1, output);
+    }
+
+    // Close files
+    fclose(input);
+    fclose(output);
+}
