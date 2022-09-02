81a2e50c99ad78832189a5344ee425535125f788
Alefer Reinert <aleferreinert@gmail.com>
2022-09-02 19:05:59 -0300

    Resolvido exercicio 1 do modulo 6.

---
+++ Modulo 6 - Python/exercicio1/mario.py
@@ -0,0 +1,10 @@
+from cs50 import get_int
+
+while True:
+    height = get_int("Height: ")
+    if height > 0 and height < 9:
+        break
+
+for i in range(height):
+    print(" " * ((height - 1) - i), end="")
+    print("#" * (i+1))
